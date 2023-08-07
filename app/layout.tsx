5d57dabe6b405b1be47fde9a6d44fc21ae473cd5
12mativ <ivan_m146@mail.ru>
2023-08-07 14:49:59 +0400

    add meta tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,29 @@ import React from 'react'
 import {Figtree} from 'next/font/google'
 import Header from '@/components/Header'
 import CustomThemeProvider from "@/providers/ThemeProvider";
+import Head from 'next/head'
 
 const font = Figtree({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Portfolio',
-  description: "Watch mativ's portfolio",
+  description: "Watch mativ's portfolio"
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
+      <Head>
+        <title>
+          Mativ's portfolio
+        </title>
+        <meta
+          name="portfolio"
+          content="Mativ's portfolio"
+          key="portfolio"
+        />
+        <meta name="google-site-verification" content="9KfTN60UutIfDgfF4fdhyP7LftxyqAqc4Snu0nVOyl0" />
+      </Head>
       <body className={font.className}>
         <CustomThemeProvider>
           <Header>{children}</Header>
