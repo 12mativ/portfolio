56f7ee5825fcdf89c352432eb59fbb10463a06a1
12mativ <ivan_m146@mail.ru>
2023-08-07 14:32:38 +0400

    change type for id in useProjects

--- hooks/useProjects.ts
+++ hooks/useProjects.ts
@@ -3,7 +3,7 @@ import {v4 as uuidv4} from 'uuid'
 import placeholderImage from '../images/1.png'
 
 export interface Project {
-  id: uuidv4
+  id: string
   name: string
   description: string
   image: {
