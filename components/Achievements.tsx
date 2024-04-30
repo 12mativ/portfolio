ee1086244783b5c01a51b348a33bed8508e8138f
12mativ <ivan_m146@mail.ru>
2024-04-30 20:50:12 +0400

    fixes 2

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -1,15 +1,16 @@
 const achievements = [
-  {id: 1, name: 'Поучаствовал в полуфинале Yandex Cup'},
-  {id: 2, name: 'Принимал участие и выигрывал в большом количестве хакатонов'},
+  {id: 1, name: '1 достижение'},
+  {id: 2, name: '2 достижение'},
 ]
 
 const Achievements = () => {
   return (
-    <div className='h-[100vh] flex flex-col items-center justify-center
-         w-auto md:w-[400px] h-[100vh] mt-auto scroll-m-6'
-         id='achievements'
+    <div
+      className='h-[100vh] flex flex-col items-center justify-center w-auto md:w-[400px] mt-auto scroll-m-6'
+      id='achievements'
     >
-      <p className='
+      <p
+        className='
         text-violet-600
         dark:text-violet-400
         text-xl
@@ -20,21 +21,14 @@ const Achievements = () => {
         dark:border-b-violet-400
         pb-6
         mb-6
-      '>
+      '
+      >
         Мои достижения
       </p>
       <div>
         {achievements.map((a) => {
           return <li key={a.id}>{a.name}</li>
         })}
-      </div>
-      <div>
-        <a
-          className='text-violet-600 dark:text-violet-400 underline'
-          href='#'
-        >
-          Ссылка на архив с сертификатами хакатонов
-        </a>
       </div>
     </div>
   )
